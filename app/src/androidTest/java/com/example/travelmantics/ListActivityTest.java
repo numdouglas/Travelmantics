e3bdf62fddaab3ca2b928b483858b3c04703c704
salitosmbogz <salitosmbogz@gmail.com>
2019-08-05 16:12:27 +0300

    new launchers and UI tests added

--- app/src/androidTest/java/com/example/travelmantics/ListActivityTest.java
+++ app/src/androidTest/java/com/example/travelmantics/ListActivityTest.java
@@ -6,14 +6,19 @@ import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+
 import org.junit.After;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static androidx.test.espresso.Espresso.pressBack;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.swipeDown;
+import static androidx.test.espresso.action.ViewActions.swipeLeft;
 import static androidx.test.espresso.action.ViewActions.swipeRight;
 import static androidx.test.espresso.action.ViewActions.swipeUp;
 import static androidx.test.espresso.action.ViewActions.typeText;
@@ -31,7 +36,7 @@ public class ListActivityTest {
 @Test
         public void findDeal() {
 
-            onView(withId(R.id.rvdeals)).perform(swipeRight());
+            onView(withId(R.id.rvdeals)).perform(swipeLeft());
             try {
                 Thread.sleep(9000);
             } catch (InterruptedException f) {
@@ -46,4 +51,54 @@ public class ListActivityTest {
                 Log.e("Sleep", "Sleep Interrupted");
             }
 
-        }}
+        }
+
+
+
+    @Test
+    public void logOut() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException f) {
+            Log.e("Sleep", "Sleep Interrupted");
+        }
+        openActionBarOverflowOrOptionsMenu(mActivityTestRule.getActivity());
+
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException f) {
+            Log.e("Sleep", "Sleep Interrupted");
+        }
+
+
+        onView(withText("Logout")).perform(click());
+
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException f) {
+            Log.e("Sleep", "Sleep Interrupted");
+        }
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException f) {
+            Log.e("Sleep", "Sleep Interrupted");
+        }
+        pressBack();
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException f) {
+            Log.e("Sleep", "Sleep Interrupted");
+        }
+
+        onView(withText("Sign in with Email")).perform(click());
+
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException f) {
+            Log.e("Sleep", "Sleep Interrupted");
+        }
+
+    }}
